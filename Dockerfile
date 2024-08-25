cb0145e820403e1cace428ebe3c8c31e4de80996
vunguyen <dzunguyenit@gmail.com>
2024-08-25 21:52:08 +0700

    d

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM ubuntu
 FROM openjdk:17-jdk-alpine
 FROM maven:3.9.4-eclipse-temurin-17-alpine
+FROM selenium/standalone-chrome
 
 WORKDIR /app
 COPY . .
-#download and install chrome
-RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #ENTRYPOINT [ "mvn", "test" ]
--- src/test/java/com/docker/dockerTesting.java
+++ src/test/java/com/docker/dockerTesting.java
@@ -3,10 +3,14 @@ package com.docker;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Unit test for simple App.
@@ -19,12 +23,21 @@ public class dockerTesting
      * @param testName name of the test case
      */
     WebDriver driver;
+    ChromeOptions chromeOptions;
 
     @BeforeClass
-    public void beforeClass()
-    {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setBrowserVersion("128.0.6613.85");
+    public void beforeClass() {
+        try {
+
+            chromeOptions = new ChromeOptions();
+            URL url = new URL("http://localhost:4444/wd/hub");
+            RemoteWebDriver driver = new RemoteWebDriver(url, chromeOptions);
+        }
+        catch (Exception e){
+            System. out.println("An error occurred: " + e.getMessage());
+
+        }
+
         driver = new ChromeDriver(chromeOptions);
     }
     @Test
